191791cb1d1f6132d11e59b5081c729a934c48b9
Slashin8r <jeklund86@gmail.com>
2020-06-03 16:33:50 -0500

    Add files via upload

---
+++ ueMarketplaceUpdate.php
@@ -0,0 +1,3 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script type="text/javascript" src="ueMarketplace.js"></script>
+<script type="text/javascript">ueMarketplace();</script>
